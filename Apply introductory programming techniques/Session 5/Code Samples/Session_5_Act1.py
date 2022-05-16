893bfc97d2791f93fedd53e41673050813f32a6c
Stampard0 <98725092+Stampard0@users.noreply.github.com>
2022-05-16 15:54:14 +0800

    Starting to Download all learning content

---
+++ Apply introductory programming techniques/Session 5/Code Samples/Session_5_Act1.py
@@ -0,0 +1,7 @@
+year = 1999
+if year >= 1991:
+    print("Python was created ")
+    print("by Guido van Rossum.")
+print("Python is a high level programming language")
+
+    
---
+++ Apply introductory programming techniques/Session 5/Code Samples/Session_5_Act2.py
@@ -0,0 +1,7 @@
+language = input("Please enter a programming language:")
+if language == "Python":
+    print("Python Language")
+else:
+    print("Not Python")
+
+    
